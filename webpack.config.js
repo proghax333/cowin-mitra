f5b073f950c8715bdad78ae834db9c605ecbff1e
Atmanand Nagpure <skillzworldtech@gmail.com>
2021-05-22 01:45:37 +0530

    Fix package json

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const path = require('path');
 module.exports = {
   target: 'node',
   entry: {
-    "telegram-bot": ['@babel/polyfill', './src/StartTelegramBot.js'],
-    "webhook-manager": ['@babel/polyfill', './src/StartWebhookManager.js'],
-    "webhook-server": ['@babel/polyfill', './src/StartWebhookServer.js'],
-    "appointment-test": ['@babel/polyfill', './src/AppointmentUtilsTest.js'],
-    "cowin-api-test": ['@babel/polyfill', './src/CoWINApiTest.js'],
+    "StartTelegramBot": ['@babel/polyfill', './src/StartTelegramBot.js'],
+    "StartWebhookManager": ['@babel/polyfill', './src/StartWebhookManager.js'],
+    "StartWebhookServer": ['@babel/polyfill', './src/StartWebhookServer.js'],
+    "AppointmentUtilsTest": ['@babel/polyfill', './src/AppointmentUtilsTest.js'],
+    "CoWINApiTest": ['@babel/polyfill', './src/CoWINApiTest.js'],
   },
   output: {
     path: path.resolve(__dirname, 'build'),
